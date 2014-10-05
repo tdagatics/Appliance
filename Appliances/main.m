53d19f3f354df6169d1659ac1555db4b9fa350c8
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-04 17:14:27 -0700

    Initial Commit

---
+++ Appliances/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Appliances
+//
+//  Created by Anthony Dagati on 10/4/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
